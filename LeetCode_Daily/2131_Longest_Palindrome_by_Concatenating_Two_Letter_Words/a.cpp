d7a4887eb8665cbfd1762ded876d9c2022c782f4
AntiTopQuark <AntiTopQuark1350@outlook.com>
2022-11-03 23:36:59 +0800

    upload: leetcode daily problem 2131 on 2022-11-03

---
+++ LeetCode_Daily/2131_Longest_Palindrome_by_Concatenating_Two_Letter_Words/a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "string.h"
+using namespace std;
+int main() {
+    string a = "ajdioa";
+    string b = "djaiodjaio";
+    cout << a.length()
+    cout <<  a+b << endl;
+}
