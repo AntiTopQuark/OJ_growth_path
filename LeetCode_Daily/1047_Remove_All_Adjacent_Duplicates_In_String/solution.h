ccc605de44765ead489fb02c30955f93aa564955
AntiTopQuark <AntiTopQuark1350@outlook.com>
2022-11-10 22:54:46 +0800

    upload:fix leetcode daily 1047

---
+++ LeetCode_Daily/1047_Remove_All_Adjacent_Duplicates_In_String/solution.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res;
+        for (char c : s) {
+            if (res.empty() || res.back() != c) {
+                res.push_back(c);
+            } else {
+                res.pop_back();
+            }
+        }
+        return res;
+    }
+};
