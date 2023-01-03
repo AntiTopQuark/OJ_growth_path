fd5df8105003200c78657d1b8c3d5a83663294f3
AntiTopQuark <AntiTopQuark1350@outlook.com>
2023-01-03 23:08:18 +0800

    update solution test case for 944

--- LeetCode_Daily/944_Delete_Columns_to_Make_Sorted/solution.cpp
+++ LeetCode_Daily/944_Delete_Columns_to_Make_Sorted/solution.cpp
@@ -6,11 +6,8 @@ using namespace std;
 
 class Solution {
 public:
+    // 思路： 实际上就是一个二维数组，从竖向看，遍历一遍就可以。
     int minDeletionSize(vector<string>& strs) {
-        if (strs.size() == 1) {
-            return 0;
-        }
-
         int res = 0;
         for(int i = 0; i < strs[0].length(); i++) {
             for(int j = 0; j < strs.size()-1; j++) {
@@ -25,6 +22,10 @@ public:
 };
 
 int main() {
-    
+    Solution s;
+    vector<string> input1 = {"cba","daf","ghi"};
+    cout << s.minDeletionSize(input1) << endl;
+    vector<string> input2 = {"a","b"};
+    cout << s.minDeletionSize(input2) << endl;
     return 0;
 }
